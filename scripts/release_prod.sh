91947f4f1a6a4ce4662af4c9b9b5d6aba5c3a628
Pontus Karlsson <pontus@sent.com>
2024-03-28 10:24:38 +0100

    amend

--- scripts/release_prod.sh
+++ scripts/release_prod.sh
@@ -5,27 +5,32 @@ command -v op >/dev/null 2>&1 || {
   exit 1
 }
 
+usage="$0 <from branch> <to branch> [<package name>]"
+
+from="$1"
+to="$2"
+if [ -z "$from" ] || [ -z "$to" ]; then
+  echo "$usage"
+fi
+
 # token="$(op read 'op://Consume/GitHub Consume Service Account Token/credential')"
 
-git pull origin prod
+git pull origin "$from"
 
 cur_origin="$(git remote get-url origin)"
 # git remote set-url origin "https://te-conbot:$token@github.com/timeedit/te-consume.git"
 
-cur_branch="$(git rev-parse --abrev-ref HEAD)"
-branch="${1:-main}"
-
-# How many commits are ahead of main in the prod branch
-ahead="$(git rev-list --left-right --count prod..."$branch" | awk '{ print $1 }')"
+# How many commits are ahead of main in the 'from' branch
+ahead="$(git rev-list --left-right --count "$from"..."$to" | awk '{ print $1 }')"
 
 if ((ahead > 0)); then
-  echo "The 'prod' branch is ahead by '$ahead' commits. Merge any quick fixes to 'prod' into '$branch' and try again."
+  echo "The '$from' branch is ahead by '$ahead' commits. Merge any quick fixes to '$from' into '$to' and try again."
   git remote set-url origin "$cur_origin"
   exit 1
 fi
 
 function question {
-  read -r -p "Are you sure you want to release prod? This will reset the 'prod' branch to '$branch'. (y/N) " answer
+  read -r -p "Are you sure you want to release '$from'? This will reset the '$from' branch to '$to'. (y/N) " answer
   case $answer in
     y | Y) ;;
     n | N | "")
@@ -40,14 +45,18 @@ function question {
 }
 question
 
-if [[ $cur_branch == "prod" ]]; then
-  (git reset --hard "$branch" \
-    && git push --force) || {
+ver=$(perl -lane 'print if s/^\s*"version":\s?"(\d+\.\d+\.\d+)",?/$1/' package.json)
+
+if [[ $to == "prod" ]]; then
+  (git checkout "$from" \
+    && git reset --hard "$to" \
+    && git tag "v$ver" \
+    && git push --tags --force) || {
     git remote set-url origin "$cur_origin"
   }
 else
-  (git checkout prod \
-    && git reset --hard "$branch" \
+  (git checkout "$from" \
+    && git reset --hard "$to" \
     && git push --force \
     && git checkout -) || {
     git remote set-url origin "$cur_origin"
