f0e6c6ee7f73c8c88b865cf9ce10d00884956a96
Pontus Karlsson <pontus@sent.com>
2024-04-02 18:02:17 +0200

    amend

--- scripts/release.sh
+++ scripts/release.sh
@@ -32,8 +32,8 @@ function post {
       local revs
       revs="$(git rev-list --left-right --count main..."$from")"
       echo "revs $revs"
-      # If the latest commit is the one we want and it's the only difference between the branches.
-      if [[ $message =~ (Bump version)|(Release to) ]] && [[ $revs =~ 0\t1 ]]; then
+      # If the latest commit is the one we want and it's the only difference between the branches
+      if [[ $message =~ (Bump version)|(Release to) ]] && [[ $revs =~ 0\s+1 ]]; then
         git checkout main \
           && git rebase "$from" \
           && git push --no-verify origin main "$from"
